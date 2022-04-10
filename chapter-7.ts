c8d4bd88d0d81c51435676e2feed2017ab52d239
Artem Senyukov <s.artman.dev@gmail.com>
2022-04-10 17:55:03 +0300

    decorators

---
+++ chapter-7.ts
@@ -0,0 +1,56 @@
+// Decorators
+
+type Constructor = new (...args: any[]) => {};
+//
+// const addMoveFunction = (Target: Constructor) => {
+//     return class extends Target {
+//         speed: number = 0;
+//
+//         move(): void {
+//             console.log('move');
+//         }
+//     }
+// }
+//
+// @addMoveFunction
+// class Horse {
+//
+//     test() {
+//
+//     }
+// }
+//
+//
+// const horse = new Horse();
+
+const measure = (target: Object, propertyKey: string, descriptor: PropertyDescriptor) => {
+    const originalMethod = descriptor.value;
+
+    descriptor.value = function () {
+        console.log('start');
+        originalMethod.apply();
+        console.log('finish');
+    };
+
+    return descriptor;
+};
+
+class Horse {
+
+    @measure
+    test() {
+        console.log('progress ... ')
+        return 'test';
+    }
+
+    @measure
+    test1() {
+        console.log('progress ... ')
+        return 'test';
+    }
+}
+
+const horse = new Horse();
+
+horse.test1();
+horse.test();
