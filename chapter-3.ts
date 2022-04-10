7bcd8e152b27f372bb0d1cd885c3f4994411cbb5
Artem Senyukov <s.artman.dev@gmail.com>
2022-04-10 13:16:50 +0300

    advanced types

--- chapter-3.ts
+++ chapter-3.ts
@@ -1,31 +1,57 @@
-type Movie = {title: string} & {duration: number};
-
-type MovieKind = 'film' | 'shortFilm';
-
-const movie: Movie = {
-    title: '',
-    duration: 123,
-}
-
-enum BookTypes {
-    humorBook = 'humor',
-    horrorBok = 'horror',
-}
-
-type Book = {title: string, type: BookTypes.humorBook, author: string} |
-    {title: string, type: BookTypes.horrorBok, pageCount: number};
-
-
-const book: Book = {
-    title: '',
-    pageCount: 99,
-    type: BookTypes.horrorBok,
-}
-
-function readBook(book: Book): void {
-    if (book.type === BookTypes.horrorBok) {
-        console.log(book.pageCount);
-    }
-}
-
-console.log(book);
+// type Movie = {title: string} & {duration: number};
+//
+// type MovieKind = 'film' | 'shortFilm';
+//
+// const movie: Movie = {
+//     title: '',
+//     duration: 123,
+// }
+//
+// enum BookTypes {
+//     humorBook = 'humor',
+//     horrorBok = 'horror',
+// }
+//
+// type Book = {title: string, type: BookTypes.humorBook, author: string} |
+//     {title: string, type: BookTypes.horrorBok, pageCount: number};
+//
+//
+// const book: Book = {
+//     title: '',
+//     pageCount: 99,
+//     type: BookTypes.horrorBok,
+// }
+//
+// function readBook(book: Book): void {
+//     if (book.type === BookTypes.horrorBok) {
+//         console.log(book.pageCount);
+//     }
+// }
+//
+// console.log(book);
+//
+//
+// // Mapped types
+//
+// interface Answer {
+//     [name: string]: number;
+// }
+//
+// const answer: Answer = { 'property': 123, 'property2': 123};
+//
+// // type FontWeight = 'bold' | 'bolder' | 'lighter';
+//
+// type FontWeightObject = {
+//     bold: number;
+//     bolder: number;
+//     lighter: number;
+// }
+//
+// type FontWeight = keyof FontWeightObject;
+//
+// type FontWeightRecord = {
+//     [fw in FontWeight]: number;
+// };
+//
+// const fw: FontWeightRecord = { 'bold': 23, bolder: 123, lighter: 34 };
+//
